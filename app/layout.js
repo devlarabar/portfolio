4d2e677ae50d1863afdb043532fc657b6a10182b
Lara Alexander <lara.alexander@hotmail.com>
2023-10-03 16:32:41 -0400

    Troubleshooting favicon

--- app/layout.js
+++ app/layout.js
@@ -4,12 +4,14 @@ import ToggleThemeProvider from '@/contexts/ThemeContext/useProvideTheme'
 import ThemeToggle from '@/components/ui/ThemeToggle'
 import Header from '@/components/ui/Header'
 import Footer from '@/components/ui/Footer'
+import Favicon from '@/public/assets/images/favicon.ico';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'Lara Alexander',
 	description: 'Software developer portfolio',
+	icons: [{ rel: 'icon', url: Favicon.src }],
 }
 
 export default function RootLayout({ children }) {
