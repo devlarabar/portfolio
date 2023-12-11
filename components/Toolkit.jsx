ed157b0fe7f3dde1871850d4594796e95e962cd7
Lara Alexander <lara.alexander@hotmail.com>
2023-12-11 16:27:37 -0500

    Added Astro, gave alt attributes to all language/tool images

--- components/Toolkit.jsx
+++ components/Toolkit.jsx
@@ -3,21 +3,22 @@ const Toolkit = () => {
     return (
         <section className="px-6 rounded-3xl max-2xs:p-4">
             <section className="languages flex flex-wrap gap-4 2xs:gap-8 justify-center content-center items-center">
-                <img src="assets/svg/tools/tool_html5.svg" className={size} />
-                <img src="assets/svg/tools/tool_css3.svg" className={size} />
-                <img src="assets/svg/tools/tool_tailwind.svg" className={size} />
-                <img src="assets/svg/tools/tool_js.svg" className={size} />
-                <img src="assets/svg/tools/tool_ts.svg" className={size} />
-                <img src="assets/svg/tools/tool_mongodb.svg" className={size} />
-                <img src="assets/svg/tools/tool_node.svg" className={size} />
-                <img src="assets/svg/tools/tool_react.svg" className={size} />
-                <img src="assets/svg/tools/tool_nextjs.svg" className={size} />
-                <img src="assets/svg/tools/tool_python.svg" className={size} />
-                <img src="assets/svg/tools/tool_django.svg" className={size} />
-                <img src="assets/svg/tools/tool_git.svg" className={size} />
-                <img src="assets/svg/tools/tool_photoshop.svg" className={size} />
-                <img src="assets/svg/tools/tool_illustrator.svg" className={size} />
-                <img src="assets/svg/tools/tool_figma.svg" className={size} />
+                <img src="assets/svg/tools/tool_html5.svg" className={size} alt="HTML5" />
+                <img src="assets/svg/tools/tool_css3.svg" className={size} alt="CSS3" />
+                <img src="assets/svg/tools/tool_tailwind.svg" className={size} alt="Tailwind CSS" />
+                <img src="assets/svg/tools/tool_js.svg" className={size} alt="JavaScript" />
+                <img src="assets/svg/tools/tool_ts.svg" className={size} alt="TypeScript" />
+                <img src="assets/svg/tools/tool_mongodb.svg" className={size} alt="MongoDB" />
+                <img src="assets/svg/tools/tool_node.svg" className={size} alt="Node.js" />
+                <img src="assets/svg/tools/tool_react.svg" className={size} alt="React" />
+                <img src="assets/svg/tools/tool_nextjs.svg" className={size} alt="Next.js" />
+                <img src="assets/svg/tools/tool_astro.svg" className={size} alt="Astro" />
+                <img src="assets/svg/tools/tool_python.svg" className={size} alt="Python" />
+                <img src="assets/svg/tools/tool_django.svg" className={size} alt="Django" />
+                <img src="assets/svg/tools/tool_git.svg" className={size} alt="Git" />
+                <img src="assets/svg/tools/tool_photoshop.svg" className={size} alt="Adobe Photoshop" />
+                <img src="assets/svg/tools/tool_illustrator.svg" className={size} alt="Adobe Illustrator" />
+                <img src="assets/svg/tools/tool_figma.svg" className={size} alt="Figma" />
             </section>
         </section>
     )
