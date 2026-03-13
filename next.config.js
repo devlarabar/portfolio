7ed61c4f6f9acca693bdc5ac09794d2d1955d862
Lara Shalab Alsham <lsalsham@hotmail.com>
2026-03-13 10:43:50 -0400

    Fix /admin redirect for local dev

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 
 const nextConfig = {
     turbopack: {},
+    async redirects() {
+        return [
+            {
+                source: "/admin",
+                destination: "/admin/index.html",
+                permanent: false,
+            },
+        ]
+    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.node/,
