b0f88cfca9934e3b8dede67dfe9d0c3c3c3ad76b
Lara Shalab Alsham <lsalsham@hotmail.com>
2026-03-13 17:18:41 -0400

    Switch CMS to GitHub OAuth backend, remove Netlify Identity

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/stars.css'
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
 import ToggleThemeProvider from '@/contexts/ThemeContext/useProvideTheme'
 import ThemeToggle from '@/components/ui/ThemeToggle'
 import Footer from '@/components/ui/Footer'
@@ -11,7 +10,7 @@ import ProgressIndicator from '@/components/ui/ProgressIndicator'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-	title: "devlarabar's blog",
+	title: 'Lara S.A. - Software Developer',
 	description: 'Full-Stack Software Developer, Game Developer, and Artist',
 	icons: [{ rel: 'icon', url: '/assets/images/favicon.ico' }],
 }
@@ -28,7 +27,7 @@ const RootLayout = ({ children, modal }) => {
 					<textarea name="message"></textarea>
 					<input name="bot-field" />
 				</form>
-				
+
 				<ToggleThemeProvider>
 					<ProgressIndicator />
 					<Navigation ThemeToggle={ThemeToggle} />
@@ -42,10 +41,6 @@ const RootLayout = ({ children, modal }) => {
 					</main>
 					<Footer />
 				</ToggleThemeProvider>
-				<Script
-					src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-					strategy="afterInteractive"
-				/>
 			</body>
 		</html>
 	)
